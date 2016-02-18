4cfec6b63b7717d285dd737c3aa8a66e1ed9277f
Rencie Bautista <rencie.bautista@yahoo.com>
2016-02-18 18:29:06 +0800

    udpate file download

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,6 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        'api/auth',
-        'api/storeaudit',
-        'api/uploadimage'
+        'api/*'
     ];
 }
